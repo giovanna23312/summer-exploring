4c0bb5e2e1903ff2815a6ff4b1f923d4c783fb3b
giovanna23312 <111871714+giovanna23312@users.noreply.github.com>
2022-09-12 11:56:57 -0300

    validação de formulario

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+/*  ///////////////////  FUNÇÕES   ////////////////////  */
+const aviso = (msg)=> {
+    alert(msg)
+}
+
+const mostraIdade = () =>{
+    let span = document.getElementById('txt-idade')
+    let campoIdade = document.getElementById ('idade')
+    span.innerText = campoIdade.value
+}
+
+
+
+
+
+/* ---------------------------------------------------- */
+
+
+
+
+
+/* //////////// EVENTOS E EXECUÇÕES AUTOMÁTICAS  /////////// */
+
+mostraIdade()
+document.getElementById('idade').addEventListener('change', mostraIdade)
+
+
+// aviso('Tenha um bom dia DAVE')
+
+// document.getElementById('nome').addEventListener('click', function(){
+//     aviso('ola mundo!!')
+// }) 
+
+
+
+//Inicializa animações scroll do AOS
+AOS.init();
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+( ()=> {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
