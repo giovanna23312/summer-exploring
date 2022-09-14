73ed47227d945e8823952aefc8e2eaca0eff639b
giovanna23312 <111871714+giovanna23312@users.noreply.github.com>
2022-09-14 11:51:17 -0300

    Funções

--- js/custom.js
+++ js/custom.js
@@ -1,91 +1,103 @@
 /*  ///////////////////  FUNÇÕES   ////////////////////  */
 const aviso = (msg)=> {
-    alert(msg)
+  alert(msg)
 }
 
-const mostraIdade = () =>{
-    let span = document.getElementById('txt-idade')
-    let campoIdade = document.getElementById ('idade')
-    span.innerText = campoIdade.value
+const mostraIdade = ()=>{
+  let span = document.getElementById('txt-idade')
+  let campoIdade = document.getElementById('idade')
+  span.innerText = campoIdade.value
 }
 
-var dataAtual = new Date
-const mostraData = () =>{
-  let dia = dataAtual.getDay()
-  let mes = dataAtual.getMonth()+1
-  let ano = dataAtual.getFullYear()
-  let hora = dataAtual.getHours()
-  let valor = dia + '/' + mes + '/' + ano + '-' + hora 
+var dataAtual = new Date()
+const monstraData = ()=> {
+let dia = dataAtual.getDay()
+let mes = dataAtual.getMonth() + 1
+let ano = dataAtual.getFullYear()
+let hora = dataAtual.getHours()
+let valor = dia +'/'+ mes + '/' + ano + ' - ' + hora
 
-  document.getElementById('dt-cadastro').value = valor 
-
+document.getElementById('dt-cadastro').value = valor
 }
 
-//preenche o select estado com os estados da api do ibge
+//Preenche o select "estado" com os estados da API do IBGE
 const getEstados = ()=>{
-  let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-  let select = document.getElementById('estado')
+let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+let select = document.getElementById('estado')
 
-  // Le a api atraves do fetch(), 1then captura os dados, o 2 then trata os dados
+//Lê a API através do fetch(), 1o then captura os dados, 2o then trata os dados
+fetch(api).then(resposta => resposta.json()).then(json => {
+  let options = '<option>Selecione</option>'
 
-    fetch(api).then(resposta => resposta.json()).then(json => {
-      let options = '<option>Selecione</option>'
-      
-      select.innerHTML = options
-    })
+  //Percorre o objeto JSON com os estados do Brasil
+  for (const index in json) {
+    options += `<option value=${json[index].sigla}>${json[index].nome}</option>`
+  }    
+  select.innerHTML = options
+})
+}
 
+
+//Preenche o select de cidades de acordo com o UF selecionado
+//A função recebe um parâmetro (uf) com a sigla da UF
+const getCidadesByUf = (uf)=>{
+let api = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+let select = document.getElementById('cidade')
+
+fetch(api).then(resposta => resposta.json()).then(json => {
+  let options = '<option>Selecione</option>'
+  
+  for (const index in json) {
+    options += `<option value=${json[index].nome}>${json[index].nome}</option>`
+  }    
+  select.innerHTML = options
+})
 
 }
+
 
 
 
 /* ---------------------------------------------------- */
 
 
-
-
-
 /* //////////// EVENTOS E EXECUÇÕES AUTOMÁTICAS  /////////// */
+
 getEstados()
 
 mostraIdade()
 document.getElementById('idade').addEventListener('change', mostraIdade)
 
-
-mostraData()
-// document.getElementById()
-
-
-// aviso('Tenha um bom dia DAVE')
-
-// document.getElementById('nome').addEventListener('click', function(){
-//     aviso('ola mundo!!')
-// }) 
-
-
+monstraData()
 
 //Inicializa animações scroll do AOS
 AOS.init();
 
-
 // Impede o envio do formulário quando os campos estão inválidos
 ( ()=> {
-    'use strict'
-  
-    // Variavel captira a tags <form> que contem a classa "needs-validation"
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Executa para cada formulario da variavel forms
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          //se houver campos invalidos, interrompe o submite 
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  //váriavel captura as tags <form> que contém a classe "needs-validation"
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Executa para cada formulário da variável forms
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        //Se houver campos inválidos, interrompe o SUBMIT
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+
+document.getElementById('estado').addEventListener('change', function(){
+getCidadesByUf(this.value)
+})
+
+
